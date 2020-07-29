9a0beabe5eff7b76a17b7bbdf8c1d15216e196bb
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-07-29 20:59:52 +0100

    article download

---
+++ download_articles.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+import click
+
+
+@click.command()
+@click.argument('url-list', type=click.Path(exists=True, dir_okay=False))
+@click.argument('download-folder', type=click.Path(file_okay=True))
+def download(url_list, download_folder):
+    url_list = Path(url_list)
+    download_folder = Path(download_folder)
+    download_folder.mkdir(parents=True, exist_ok=True)
+    downloaded_urls = download_folder / "fetched.txt"
+
+    downloaded_set = set()
+    old_downloaded = []
+
+    if downloaded_urls.exists():
+        with open(downloaded_urls) as reader:
+            for line in reader:
+                aux = line.strip()
+                downloaded_set.add(aux)
+                old_downloaded.append(aux)
+
+    new_downloaded = []
+    with open(url_list) as reader:
+        for line in reader:
+            url = line.strip()
+            if url in downloaded_set:
+                break
+            new_downloaded.append(url)
+            _ , _, name = url.rpartition("/")
+            urlretrieve(url, download_folder / f"{name}.html")
+
+    all_urls = new_downloaded + old_downloaded
+
+    with open(downloaded_urls, "w") as writable:
+        for url in all_urls:
+            writable.write(url + "\n")
+
+
+if __name__ == "__main__":
+    download()
+
+
+
+
