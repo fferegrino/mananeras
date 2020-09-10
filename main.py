7f3842a1b4a5d992c31a9b0f87d5eddeda87d2d8
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-09-10 22:28:16 +0100

    automatic dataset creation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from utils.download_urls import download_urls
 from utils.download_articles import download_articles
 from utils.extract_dialogs import extract
+from kaggle import api
+import shutil
 
 
 def main():
     download_urls("urls.txt", 1)
     download_articles("urls.txt", "raw")
     extract("raw", "articulos")
+    shutil.make_archive('data/articulos', 'zip', "articulos")
+    api.dataset_create_version("data", "Daily dataset update", dir_mode="zip", quiet=False)
 
 
 if __name__ == "__main__":
